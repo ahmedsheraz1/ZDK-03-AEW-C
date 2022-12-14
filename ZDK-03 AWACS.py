7bc71b0edaf9c31bc7eb83ded207557487be32e4
Ahmed Sheraz <ahmedsheraz1@gmail.com>
2022-12-14 07:18:40 +0500

    Create ZDK-03 AWACS.py

---
+++ ZDK-03 AWACS.py
@@ -0,0 +1,5 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+from hashlib import blake2b
+blake2b(b'ZDK-03 AEW&C').hexdigest()
+'78a223d574b01b418b8bdea2b92129b9838210d553afb9850af71920bac83e4ece8fa0922ef2588cf6b5d97504834889adf4a245b1d6f4d58cf2b9487e31c432'
